e7273ad70f14ed11d4d8450bcfe70178d6128f4f
Iswar <rick731202@gmail.com>
2021-03-02 21:36:35 +0530

    Fixed the issue of clicking the start button to get more click per second score by disabling the start button after a single tap, added a reset button at the bottom

--- script/logic.js
+++ script/logic.js
@@ -1,8 +1,15 @@
+// Declear the global variables
 let clickvar = 0;
 window.clicks = 0;
+
+// Setup the start button function
 let startfunction = () => {
     window.time = new Date();
+    document.querySelector("#start").disabled = true;
+    document.querySelector("#start").style.backgroundColor = "gray"
 };
+
+// Click per second counter function
 let clickfunction = () => {
     clickvar += 1;
     clicks = clickvar;
@@ -12,6 +19,14 @@ let clickfunction = () => {
     // console.log(timeDiff)
     window.clickPerSecond = Math.floor(clicks / timeDiff);
     // console.log(clickPerSecond)
-    document.querySelector("#result").innerHTML = `You CPS is ${clickPerSecond}`;
-    document.querySelector("#result").style.display = "inline-block"
+    document.querySelector("#result").innerHTML = `Your Click Per Second is ${clickPerSecond}`;
+    // document.querySelector("#result").style.display = "inline-block"
 };
+
+// Reset function
+const resetfunction = () => {
+    clicks = 0;
+    document.querySelector("#result").innerHTML = `Your Click Per Second is `;
+}
+
+document.querySelector("#reset").onclick = resetfunction;
